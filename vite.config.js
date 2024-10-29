9975226d6ad81a8c5ff728b7fbce8a7c33fe5655
HAMALAWY\Lenovo <te7a.12345@gmail.com>
2024-10-29 21:31:22 +0300

    2nd upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
+  base: "/deploy-Ecommerce-vite/", // Add this line
   server: { port: 3000 },
   define: { _global: {} },
 });
