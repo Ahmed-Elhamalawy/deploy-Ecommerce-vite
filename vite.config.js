27abc556a2eba8fb5bce4b6e25fb42b38c792724
HAMALAWY\Lenovo <te7a.12345@gmail.com>
2024-10-29 21:49:23 +0300

    4th

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/deploy-Ecommerce-vite/",
-  server: { port: 3000 },
-  define: { _global: {} },
   build: {
     outDir: "dist",
-    sourcemap: true,
+    assetsDir: "assets",
+    manifest: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[hash].[ext]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
   },
+  server: { port: 3000 },
+  define: { _global: {} },
 });
